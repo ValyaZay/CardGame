717cadbaedd467c5b077d54f7e0bac95c5330881
Valya Zaitseva <valentyna.zaitseva@bini.games>
2021-01-28 15:57:51 +0200

    Finish safe area for button

--- Assets/ValZay.CardGame/Scripts/SafeArea.cs
+++ Assets/ValZay.CardGame/Scripts/SafeArea.cs
@@ -8,11 +8,6 @@ namespace ValZay.CardGame
 {
     public class SafeArea : MonoBehaviour
     {
-        [SerializeField] private bool stretchIfNotchedDevice;
-        [SerializeField] private GameObject header;
-        [SerializeField] private float headerPositionForNotchedDevices = -77f;
-        [SerializeField] private float headerPositionForUsualDevices = 18f;
-        
         private RectTransform panelSafeArea;
         
         private void Awake()
@@ -35,26 +30,7 @@ namespace ValZay.CardGame
             if (panelSafeArea == null)
                 return;
             
-            Rect safeArea = Screen.safeArea;
-
-            if (stretchIfNotchedDevice)
-            {
-
-
-                if (safeArea.height < Screen.height)
-                {
-                    safeArea.y = 0;
-                    safeArea.height = Screen.height;
-
-                    AlignHeader(headerPositionForNotchedDevices);
-                }
-                else
-                {
-                    AlignHeader(headerPositionForUsualDevices);
-                }
-            }
-
-            //ConvertPixelsToNormalizedCoordinate(safeArea);
+            ConvertPixelsToNormalizedCoordinate(Screen.safeArea);
         }
 
         private void ConvertPixelsToNormalizedCoordinate(Rect safeArea)
@@ -70,15 +46,6 @@ namespace ValZay.CardGame
 
             panelSafeArea.anchorMin = anchorMin;
             panelSafeArea.anchorMax = anchorMax;
-        }
-
-        private void AlignHeader(float anchoredPosY)
-        {
-            if(!header)
-                return;
-            var rectTrans = header.GetComponent<RectTransform>();
-            var rectY = anchoredPosY;
-            rectTrans.anchoredPosition = new Vector2(0, rectY);
         }
     }
 }
