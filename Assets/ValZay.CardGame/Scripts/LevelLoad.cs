a76821cabc1c6e243961de8d3b55ab73576aab13
Valya Zaitseva <valentyna.zaitseva@bini.games>
2021-01-28 07:11:26 +0200

    Add Splash screen and level start

---
+++ Assets/ValZay.CardGame/Scripts/LevelLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ValZay.CardGame
+{
+    public class LevelLoad : MonoBehaviour
+    {
+        [SerializeField] private float waitTillStart = 3.0f;
+
+        private int startSceneIndex = 1;
+        
+        IEnumerator Start()
+        {
+            yield return new WaitForSeconds(waitTillStart);
+            LoadStartScene();
+        }
+
+        private void LoadStartScene()
+        {
+            SceneManager.LoadScene(startSceneIndex);
+        }
+    }
+}
