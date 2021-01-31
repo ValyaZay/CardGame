6b212b96e7c69f15c420da61adcfd592e0056277
Valya Zaitseva <valentyna.zaitseva@bini.games>
2021-01-31 14:54:54 +0200

    Add table canvases

---
+++ Assets/ValZay.CardGame/Scripts/CustomTablePrefs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace ValZay.CardGame
+{
+    public class CustomTablePrefs : MonoBehaviour
+    {
+        const string TABLE_SPRITE_KEY = "table";
+        const int TABLE_SPRITE = 0;
+
+        [SerializeField] private Button button;
+        [SerializeField] private Canvas dialogCanvas;
+        
+
+        private void Start()
+        {
+            button.onClick.AddListener(ShowDialogCanvas);
+        }
+
+        private void ShowDialogCanvas()
+        {
+            dialogCanvas.gameObject.SetActive(true);
+    
+        }
+
+        public static void SetTable(int table)
+        {
+            PlayerPrefs.SetInt(TABLE_SPRITE_KEY, table);
+        }
+
+        public static int GetTable()
+        {
+            return PlayerPrefs.GetInt(TABLE_SPRITE_KEY);
+        }
+    }
+}
